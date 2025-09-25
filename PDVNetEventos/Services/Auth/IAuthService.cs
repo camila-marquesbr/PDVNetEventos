bd313ba02d39a8e34614314f8f596cc075892564
Camila Marques <camiladev.br@gmail.com>
2025-09-25 19:17:59 -0300

    autenticacao" ""

---
+++ PDVNetEventos/Services/Auth/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDVNetEventos.Services.Auth
+{
+    public interface IAuthService
+    {
+        /// <summary>Valida credenciais e retorna um "token" mock se ok.</summary>
+        string? Login(string username, string password);
+
+        /// <summary>Usuário atual (memoriza após login).</summary>
+        string? CurrentUser { get; }
+        string? CurrentToken { get; }
+        void Logout();
+    }
+}
